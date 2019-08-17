fd9f4c723acf816083405e0a4e9d90dac2d9618a
PREETHI33preethi <48052863+PREETHI33preethi@users.noreply.github.com>
2019-08-17 06:39:18 +0530

    Create Min.c

---
+++ Min.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,min,N,number[50];
+clrscr();
+scanf("%d",&n);
+for(i=0;i<N;i++)
+{
+scanf("%d",&number[i]);
+}
+min=number[0];
+for(i=0;i<N;i++)
+{
+if(min>number[i])
+{
+min=number[i];
+}
+}
+printf("%d",min);
+getch();
+}
